72c66740bd0c68d8cc45acdda36997c48057c6a1
Hector Toro <alejotoro94@hotmail.com>
2023-05-20 18:12:02 -0500

    Se sube los modelos Dog y Temperament, adicional se hace la relacion de muchos a muchos con entidad dogs-temperaments

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,10 +3,35 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('Dog', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weigth: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    yearsLife: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
   });
 };
---
+++ api/src/models/Temperament.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+
+
+module.exports = (sequelize) => {
+    sequelize.define("Temperament", {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        timestamps: false,
+    });
+};
+
+
