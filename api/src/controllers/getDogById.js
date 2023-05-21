d3307d6c11c58262941a1a76e98c0317652a4e0b
Hector Toro <alejotoro94@hotmail.com>
2023-05-21 17:32:59 -0500

    Se sube el controlador por id, solo falta validar como enviar en los dogs de DB también sus temperaments cuando se consulto por esos perros

--- api/src/controllers/getDogById.js
+++ api/src/controllers/getDogById.js
@@ -1,8 +1,21 @@
-
+const axios = require("axios");
+const {API_KEY, URL} = require("../utils/globalsVar");
+const { Dog } = require("../db");
 
 const getDogById = async (req,res)=> {
-    const {id} = req.params;
-    res.status(200).send("bienvenido a la card del ID: " + id);
+    try {
+        const {id} = req.params;
+        const response = await axios(`${URL}${API_KEY}`); 
+        const dogsData = response.data;
+        const dogsIdApi = dogsData.find( dog => dog.id === Number(id));
+        let dogsIdDB;
+        if(!dogsIdApi) {
+            dogsIdDB = await Dog.findByPk(id);
+        }
+        res.status(200).json(dogsIdApi || dogsIdDB);
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    }
 }
 
 module.exports = getDogById;
